94e340c2e450d575eeea54c3c92436cb7e5df9e9
Arvid Edenheim <arved490@student.liu.se>
2019-09-22 16:15:31 +0200

    Adds audio support cross-browser

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,9 +25,9 @@ window.onload = () => {
     const isChrome =
         /Chrome/.test(navigator.userAgent) &&
         /Google Inc/.test(navigator.vendor)
-    if (!isChrome) {
-        $('#iframeAudio').remove()
-    } else {
-        $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background
-    }
+
+    const node = document.querySelector(
+        isChrome ? '#playAudio' : '#iframeAudio'
+    )
+    node.remove()
 }
