fc910c1875ab834096712989c23bb121cbd21b4e
Arvid Edenheim <arved490@student.liu.se>
2020-01-16 10:14:10 +0100

    update with kröktext

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,38 @@
-window.onload = () => {
-    function renderStars() {
-        function getRandomPosition() {
-            const y = window.innerWidth
-            const x = window.innerHeight
-            const randomX = Math.floor(Math.random() * x)
-            const randomY = Math.floor(Math.random() * y)
-            return [randomX, randomY]
-        }
-
-        const numStars = 100
-
-        for (let i = 0; i < numStars; i++) {
-            let star = document.createElement('div')
-            star.className = 'star'
-            const xy = getRandomPosition()
-            star.style.top = xy[0] + 'px'
-            star.style.left = xy[1] + 'px'
-            document.body.append(star)
-        }
+function renderStars() {
+    function getRandomPosition() {
+        const y = window.innerWidth
+        const x = window.innerHeight
+        const randomX = Math.floor(Math.random() * x)
+        const randomY = Math.floor(Math.random() * y)
+        return [randomX, randomY]
     }
 
+    const numStars = 100
+
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.className = 'star'
+        const xy = getRandomPosition()
+        star.style.top = xy[0] + 'px'
+        star.style.left = xy[1] + 'px'
+        document.body.append(star)
+    }
+}
+
+
+toggleContent = () => {
     renderStars()
 
-    const isChrome =
-        /Chrome/.test(navigator.userAgent) &&
-        /Google Inc/.test(navigator.vendor)
+    const node = document.getElementById("start-button");
+    node.className = 'active'
 
-    const node = document.querySelector(isChrome ? '#playAudio' : '#iframeAudio')
-    node.remove()
-
-    document.body.click()
+    if (node.parentNode) {
+        setTimeout(() => {
+            node.parentNode.removeChild(node)
+            const content = document.querySelector('#template')
+            const copy = document.importNode(content.content, true)
+            document.body.append(copy)
+        }, 1000)
+    }
 }
+
